3a24003293b47e28e9dbac75510069796ec9ad2a
Sergio D. Barrera <sdbeng@gmail.com>
2021-04-24 23:03:45 -0700

    update shuffle logic

--- card_oop.js
+++ card_oop.js
@@ -1,4 +1,4 @@
-class Card{
+class Deck{
     constructor(){
         this.deck=[]
         const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
@@ -13,18 +13,32 @@ class Card{
     }
     //shuffle method
     shuffle(){
-        const {deck} = this
-        let c = deck.length, i
-        //loop over while c is valid and decrement i
-        while(c){
-            i = Math.floor(Math.random() * c--)
-            //swap array indices
-            [deck[c], deck[i]] = [deck[i], deck[c]]
+        //another method to shuffle
+        let loc1, loc2, temp;
+        for(let i=0; i<1000; i++){
+            loc1=Math.floor(Math.random()*this.deck.length)
+            loc2=Math.floor(Math.random()*this.deck.length)
+            //swap indices
+            temp = this.deck[loc1];
+            this.deck[loc1] = this.deck[loc2];
+            this.deck[2]=temp;
         }
-        return this
+
+        // //destructure this.deck
+        // const {deck} = this
+        // let c = deck.length, i
+        // //loop over while c is valid and decrement i
+        // while(c){
+        //     i = Math.floor(Math.random() * c--)
+        //     //swap array indices
+        //     //i get TypeError: Cannot create property 'undefined', it probably this es7 syntax is not yet valid in node.js
+        //     [deck[c], deck[i]] = [deck[i], deck[c]]
+        // }
+        // return this
     }
 }
 
-const card1 = new Card()
+const card1 = new Deck()
 console.log(card1)
 card1.shuffle()
+console.log(card1)
